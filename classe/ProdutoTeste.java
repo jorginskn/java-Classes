8cb9808d059706a266f32a9c7767614787485fc7
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2023-02-06 10:39:13 -0300

    feat: Classe produto instancia de nome preco e desconto

--- classe/ProdutoTeste.java
+++ classe/ProdutoTeste.java
@@ -7,16 +7,21 @@ public class ProdutoTeste {
 		  
 		 Produto p1 = new Produto();
 		 p1.nome = "Jorge";
-		 p1.preco = 2900;
+		 p1.preco = 4356.89;
 		 p1.desconto = 0.25;
 		 
 		 
 		 var p2 = new Produto();
-		 p2.nome= " O pica";
-		 p2.preco= 100;
-		 p2.desconto = 0.10;
+		 p2.nome= " Caneta preta";
+		 p2.preco= 12.56;
+		 p2.desconto = 0.29;
 		 
-		 p2.preco = p2.preco /  p2.desconto;
-		 System.out.print(p2.preco);
-	}
+ 		 
+ 		 
+ 		 
+ 		 double precoFinal1 = p1.preco *(1 - p1.desconto);
+ 		 double precoFinal2 = p2.preco *(1 - p2.desconto);
+ 		 double mediaCarrinho = (precoFinal1 + precoFinal2) / 2;
+ 		 System.out.println(mediaCarrinho);
+ 	}
 }
